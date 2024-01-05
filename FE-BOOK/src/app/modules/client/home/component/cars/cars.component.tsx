40ad8eecc6c5147c2d9291a7521b815f336f9121
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2024-01-05 21:24:40 +0700

    emoution css

--- FE-BOOK/src/app/modules/client/home/component/cars/cars.component.tsx
+++ FE-BOOK/src/app/modules/client/home/component/cars/cars.component.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import { css } from '@emotion/react'
 import SwiperListComponent from '~/app/component/stacks/swiper-list/swiper-list.component'
 
 const CarsComponents = () => {
     return (
-        <div>
+        <div css={carsCss}>
             <SwiperListComponent title='CARS' />
-            <p className='text-red-600'>Slide show</p>
+            <p className='text-one'>Slide show</p>
         </div>
     )
 }
 
 export default CarsComponents
+
+const carsCss = css`
+.text-one{
+    color: black;
+    font-size:33px;
+}
+
+@media (min-width: 0) and (max-width: 739px) {
+    .text-one{
+        color: pink;
+        font-size:13px;
+    }
+} 
+`
