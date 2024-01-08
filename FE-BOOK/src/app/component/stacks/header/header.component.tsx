7bf6aba04210cf26b3ecefc37a18983ddd5a4ec6
quanglinh <linhdqph27370@fpt.edu.vn>
2024-01-09 01:23:55 +0700

    update header

--- FE-BOOK/src/app/component/stacks/header/header.component.tsx
+++ FE-BOOK/src/app/component/stacks/header/header.component.tsx
@@ -5,64 +5,46 @@ import { FaInstagramSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { css } from '@emotion/react';
 import logo from '../../../../assets/img/logo_book_bus.png'
+import { Button } from 'antd';
+import ButtonRadiusCompoennt from '../../parts/button/button.component';
+
 const HeaderComponent = () => {
     return (
         <>
-            <header css={headerCss} className='bg-orange-500 h-[150px] fixed w-full z-20 top-0 left-0 '>
-                <div className="header-container w-[1128px] m-auto pt-1">
-                    <nav className='header__socical'>
-                        <ul className='header__socical-list '>
-                            <li className='header__socical-link'><Link to="/facebook"><FaFacebookSquare /></Link></li>
-                            <li className='header__socical-link'><Link to="/instagram"><FaInstagramSquare /></Link></li>
-                            <li className='header__socical-link'><Link to={"/linkin"}><FaLinkedin /> </Link></li>
-                        </ul>
-                    </nav>
-                    <div className="header__logo">
-                        <Link to={'/'}><img src={logo} alt="" width={80} /></Link>
-                    </div>
-
-                    <div className="header__user">
-                        <button className='header__user--login btn'>Log In</button>
-                        <button className='header__user--resgister btn'>Sign Up</button>
-                    </div>
-
+            <header css={headerCss} className=' bg-gradient-to-b from-bgHeader to-primary h-[120px] fixed w-full z-20 top-0 left-0 '>
+            <div className="header-container w-[1128px] m-auto py-6">
+                <div className="header__logo ">
+                    <Link className='flex justify-center items-center' to="/"><img className='w-[70px]' src={logo} alt="BookBus Logo" /> <h3 className='ml-2 font-semibold text-[20px]'>LetGo5</h3></Link>
+                    
                 </div>
-                <div className='navMenu m-[auto]:'>
-                    <ul className='navMenu__list'>
-                        <li className='navMenu__list-link'><Link to={'/'} >Trang chủ</Link></li>
-                        <li className='navMenu__list-link'><Link to={'/'}>Lịch trinh</Link></li>
-                        <li className='navMenu__list-link'><Link to={'/check-ticket'}>Tra cứu vé</Link></li>
-                        <li className='navMenu__list-link'><Link to={'/'}>Hóa đơn</Link></li>
-                        <li className='navMenu__list-link'><Link to={'/'}>Liên hệ</Link></li>
-                        <li className='navMenu__list-link'><Link to={'/'}>Về chúng tôi</Link></li>
+                <nav>
+                <ul className='navMenu__list'>
+                        <li className='navMenu__list-link hover-underline-animation underline-animation--active'><Link to={'/'} >Trang chủ</Link></li>
+                        <li className='navMenu__list-link hover-underline-animation'><Link to={'/'}>Lịch trinh</Link></li>
+                        <li className='navMenu__list-link hover-underline-animation'><Link to={'/check-ticket'}>Tra cứu vé</Link></li>
+                        <li className='navMenu__list-link hover-underline-animation'><Link to={'/'}>Hóa đơn</Link></li>
+                        <li className='navMenu__list-link hover-underline-animation'><Link to={'/'}>Liên hệ</Link></li>
+                        <li className='navMenu__list-link hover-underline-animation'><Link to={'/'}>Về chúng tôi</Link></li>
                     </ul>
+                </nav>
+                <div className="header__user flex justify-end items-center ">
+                    <div>
+                        <div className="btn text-[12px]">Đăng kí/ Đăng nhập</div>
+                    </div>
                 </div>
+            </div>   
             </header>
-
         </>
     )
 }
 
 export default HeaderComponent
 const headerCss = css`
+
 .header-container{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-.header__socical{
-    display: inline-block;
-    margin-right:100px
-}
-.header__socical-list{
-    list-style: none;
-    display: flex;
-    gap: 15px;
-    font-size: 16px;
-    color: white;
-}
-.navMenu{
-   margin-top:20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .navMenu__list{
     display: flex;
@@ -70,34 +52,52 @@ const headerCss = css`
     align-items: center;
 }
 .navMenu__list-link{
-    padding: 0px 32px ;
+    padding: 0px 22px ;
     border-radius: 7px;
     font-weight:600;
-    font-size: 1.4rem;
+    font-size: 1rem;
     color: white;
     transition: all .3s ease;
     &:hover {
-        background-color: #f9a63d;
-        box-shadow: inset -8px -8px 0 rgba(255 255 255 / .5), inset 8px 8px0 rgba(0, 0, 0 / .5);
-        box-shadow: 0 0 10px rgba(0,0,0,.2);
+        // background-color: #f9a63d;
+        // box-shadow: inset -8px -8px 0 rgba(255 255 255 / .5), inset 8px 8px0 rgba(0, 0, 0 / .5);
+        // box-shadow: 0 0 10px rgba(0,0,0,.2);
     }
 }
-.header__logo{
-    justify-self: center;
-    margin-left: auto;
-    margin-right: auto;
-}
-.btn:first-child{
-    margin-right: 8px;
-}
+.hover-underline-animation {
+    display: inline-block;
+    position: relative;
+  }
+  
+  .hover-underline-animation::after,
+  .hover-underline-animation:active::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 4px;
+    bottom: -10px;
+    left: 0;
+    background-color: #2563eb;
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+  
+  .hover-underline-animation:hover::after,
+  .hover-underline-animation:active::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+  
+
 .btn{
     text-transform: uppercase;
     cursor: pointer;
     outline: none;
     border: none;
     color: #000;
-    padding: 8px 16px;
-    font-weight: bold;
+    padding: 5px 10px;
+    font-weight: 600;
     border-radius: 7px;
     /*background-image: linear-gradient(to right,#ffc08b ,
         #fbaf41);*/
--- FE-BOOK/tailwind.config.js
+++ FE-BOOK/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0284c7",
+        secondary: "#1d8348",
+        success: "#5cb85c",
+        danger: "#d9534f",
+        bgHeader: '#38bdf8'
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      backgroundColor: {
+        main: '#0284c7',
+        overlay: 'rgba(0,0,0,0.5)'
+      },
+    },
+
   },
   plugins: [],
 }
