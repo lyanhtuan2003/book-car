7380d5f522b43031701e72aae0309e6c779192f5
lyanhtuan2003~ <lyanhtuan21072003@gmail.com>
2024-01-12 23:22:46 +0700

    demo form

--- FE-BOOK/src/app/modules/admin/banner/banner-admin.component.tsx
+++ FE-BOOK/src/app/modules/admin/banner/banner-admin.component.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import TemplateTable from '../common/template-table/template-table.component'
+import { Form, Select, Upload } from 'antd'
+const { Option } = Select
 const BannerAdmin = () => {
     return (
-        <div>BannerAdmin</div>
+        <div>
+            <TemplateTable
+                formEdit={
+                    <Fragment>
+                        <Form.Item
+                            label='Images'
+                            name='images'
+                            getValueFromEvent={(event) => event.fileList}
+                            rules={[{ required: true, message: 'Đây là trường bắt buộc' }]}
+                            valuePropName={'fileList'}
+                        >
+                            <Upload listType='picture-card' >
+                                + Upload
+                            </Upload>
+                        </Form.Item>
+                        <Form.Item
+                            label='Trạng thái'>
+                            <Select placeholder="lựa chọn trạng thái">
+
+                                <Option value="0" >Áp dụng</Option>
+                                <Option value="1" >Vô hiệu hoá</Option>
+
+                            </Select>
+                        </Form.Item>
+
+                    </Fragment>
+                }
+            />
+        </div>
     )
 }
 
